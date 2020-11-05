5f2099a026aba0e3e35884b23d24d37c0e7d8baa
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2020-11-05 22:26:28 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,8 +13,8 @@ end
 
 def get_japanese_emoticon(file, e_e)
   new_hash2 = load_library(file)
-  binding.pry
   new_hash2.each do |k, v| 
+    binding.pry
       if k[v][:english] == e_e
         return k[v][:japanese]
       else
