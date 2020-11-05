6165c3dbc9b1cab8169392bdd6458bd7254a6494
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2020-11-05 22:51:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,10 +13,10 @@ def load_library(file)
   new_hash
 end
 
-def get_japanese_emoticon(file, e_e)
+def get_japanese_emoticon(file, e)
   new_hash2 = load_library(file)
   emoticon = new_hash2.keys.find do |k| 
-      new_hash2[k][:english] == e_e
+      new_hash2[k][:english] == e
   end
   emoticon ? new_hash2[emoticon][:japanese] : puts "Sorry"
 end
