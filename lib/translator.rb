dab24ab543756ca5e1dcdf4600da6343dcc582aa
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2020-11-05 22:50:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,13 +15,10 @@ end
 
 def get_japanese_emoticon(file, e_e)
   new_hash2 = load_library(file)
-  new_hash2.keys.find do |k| 
-      if k[v][:english] == e_e
-        return k[v][:japanese]
-      else
-        puts "Sorry, that emoticon was not found"
-      end
-    end
+  emoticon = new_hash2.keys.find do |k| 
+      new_hash2[k][:english] == e_e
+  end
+  return new_hash2[emoticon][:japanese]
 end
 
 def get_english_meaning (file, emoticon)
